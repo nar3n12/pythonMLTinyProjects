19fd8c19a41a05605f8d51202d6c81327152c7fb
Narendra Kumar <30648034+nar3n12@users.noreply.github.com>
2018-10-27 18:12:05 +0530

    naahhh..

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -3,7 +3,7 @@
 
 # In[39]:
 
-
+print("### BMI CALCULATOR ###")
 from sklearn import tree
 #x=[height,weight]
 X =[[160,60],[160,40],[160,90],[150,50],[150,40],[150,60],[170,70],[170,40],[170,100],[155,55],[155,30],[155,70]]
@@ -20,5 +20,5 @@ print(prediction)
 # In[35]:
 
 
-print("rahul")
+print("- Rahul")
 
